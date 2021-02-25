a2aec102e0e2bf9831b978389be0440b4285b73a
Yusuke Kubota <yusuke.kubota@gmo.media>
2021-02-25 20:29:47 +0900

    Issueクローズ

--- TodoTest.java
+++ TodoTest.java
@@ -1,6 +1,5 @@
 public class TodoTest {
-    // TODO はろわをHello Worldに変える
     public static void main(String[] args) {
-        System.out.println("はろわ");
+        System.out.println("Hello World");
     }
 }
